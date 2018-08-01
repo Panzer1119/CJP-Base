005920be8edb81e2c12192b3348ef00f47fc43f4
Paul Hagedorn <panzer1119@gmail.com>
2018-08-01 18:06:53 +0200

    Some testing
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/io/file/t1/TestAdvancedFile.java
+++ src/main/java/de/codemakers/io/file/t1/TestAdvancedFile.java
@@ -18,6 +18,46 @@ package de.codemakers.io.file.t1;
 
 public class TestAdvancedFile {
 
-
-
+    public static final String FILE_SEPARATOR_WINDOWS = "\\";
+    public static final String FILE_SEPARATOR_UNIX = "/";
+    
+    private PathEntry path = null;
+    private final String separator;
+    
+    public TestAdvancedFile(String path) {
+        boolean isUnix = path.contains(FILE_SEPARATOR_UNIX);
+        if (isUnix && !path.contains(FILE_SEPARATOR_WINDOWS)) {
+            separator = FILE_SEPARATOR_UNIX;
+        } else if (!isUnix && path.contains(FILE_SEPARATOR_WINDOWS)) {
+            separator = FILE_SEPARATOR_WINDOWS;
+        } else {
+            throw new RuntimeException("Path may not contain Unix and Windows file separators at the same time");
+        }
+        final String[] split = path.split(isUnix ? separator : separator + separator);
+        for (String p : split) {
+            if (this.path == null) {
+                this.path = new PathEntry(p, false);
+            } else {
+                this.path = new PathEntry(this.path, p, p.contains("."));
+            }
+        }
+    }
+    
+    public final PathEntry getPath() {
+        return path;
+    }
+    
+    public final String getSeparator() {
+        return separator;
+    }
+    
+    public final String getPathString() {
+        return path.toPathString(getSeparator());
+    }
+    
+    @Override
+    public String toString() {
+        return "TestAdvancedFile{" + "path=" + path + ", separator='" + separator + '\'' + '}';
+    }
+    
 }
