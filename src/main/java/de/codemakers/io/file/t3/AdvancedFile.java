c1412530304dcfc67e07a3ed90e87647b9daaab0
Paul Hagedorn <panzer1119@gmail.com>
2018-08-21 21:39:17 +0200

    Complete New AdvancedFile.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/io/file/t3/AdvancedFile.java
@@ -0,0 +1,237 @@
+/*
+ *     Copyright 2018 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.io.file.t3;
+
+import de.codemakers.base.os.OSUtil;
+import de.codemakers.io.file.t3.exceptions.*;
+
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AdvancedFile implements IFile {
+    
+    public static final String FILE_SEPARATOR_WINDOWS_STRING = OSUtil.WINDOWS_HELPER.getFileSeparator();
+    public static final String FILE_SEPARATOR_DEFAULT_STRING = OSUtil.DEFAULT_HELPER.getFileSeparator();
+    public static final String FILE_SEPARATOR_CURRENT_STRING = OSUtil.CURRENT_OS_HELPER.getFileSeparator();
+    public static final String FILE_SEPARATOR_NOT_CURRENT_STRING = FILE_SEPARATOR_CURRENT_STRING == FILE_SEPARATOR_WINDOWS_STRING ? FILE_SEPARATOR_DEFAULT_STRING : FILE_SEPARATOR_WINDOWS_STRING;
+    public static final char FILE_SEPARATOR_WINDOWS_CHAR = OSUtil.WINDOWS_HELPER.getFileSeparatorChar();
+    public static final char FILE_SEPARATOR_DEFAULT_CHAR = OSUtil.DEFAULT_HELPER.getFileSeparatorChar();
+    public static final char FILE_SEPARATOR_CURRENT_CHAR = OSUtil.CURRENT_OS_HELPER.getFileSeparatorChar();
+    public static final char FILE_SEPARATOR_NOT_CURRENT_CHAR = FILE_SEPARATOR_CURRENT_CHAR == FILE_SEPARATOR_WINDOWS_CHAR ? FILE_SEPARATOR_DEFAULT_CHAR : FILE_SEPARATOR_WINDOWS_CHAR;
+    public static final String FILE_SEPARATOR_WINDOWS_REGEX = OSUtil.WINDOWS_HELPER.getFileSeparatorRegex();
+    public static final String FILE_SEPARATOR_DEFAULT_REGEX = OSUtil.DEFAULT_HELPER.getFileSeparatorRegex();
+    public static final String FILE_SEPARATOR_CURRENT_REGEX = (FILE_SEPARATOR_CURRENT_CHAR == FILE_SEPARATOR_WINDOWS_CHAR) ? FILE_SEPARATOR_WINDOWS_REGEX : FILE_SEPARATOR_DEFAULT_REGEX;
+    public static final String FILE_SEPARATOR_NOT_CURRENT_REGEX = (FILE_SEPARATOR_CURRENT_CHAR != FILE_SEPARATOR_WINDOWS_CHAR) ? FILE_SEPARATOR_WINDOWS_REGEX : FILE_SEPARATOR_DEFAULT_REGEX;
+    protected String[] paths;
+    private boolean windowsSeparator = true;
+    private boolean extern = true;
+    private boolean absolute = true;
+    private AdvancedFile parent;
+    private FileProvider fileProvider;
+    private String path;
+    
+    public AdvancedFile(String... paths) {
+        this.paths = paths;
+    }
+    
+    protected AdvancedFile(AdvancedFile parent, FileProvider fileProvider, String[] paths) {
+        this.parent = parent;
+        this.fileProvider = fileProvider;
+        this.paths = paths;
+    }
+    
+    @Override
+    public String getName() {
+        if (paths.length == 0) {
+            return "";
+        }
+        return paths[paths.length - 1];
+    }
+    
+    @Override
+    public String getPath() {
+        if (path == null) {
+            generatePath();
+        }
+        if (parent != null) {
+            return parent.getPath() + getSeparatorChar() + path;
+        }
+        return path;
+    }
+    
+    private final String generatePath() {
+        path = Stream.of(paths).collect(Collectors.joining(getSeparator()));
+        return path;
+    }
+    
+    @Override
+    public String getAbsolutePath() {
+        return null;
+    }
+    
+    @Override
+    public AdvancedFile getAbsoluteFile() {
+        return null;
+    }
+    
+    @Override
+    public AdvancedFile getParentFile() {
+        return null;
+    }
+    
+    @Override
+    public Path toPath() throws Exception {
+        return null;
+    }
+    
+    @Override
+    public URI toURI() throws Exception {
+        return null;
+    }
+    
+    @Override
+    public URL toURL() throws Exception {
+        return null;
+    }
+    
+    @Override
+    public String getSeparator() {
+        return windowsSeparator ? FILE_SEPARATOR_WINDOWS_STRING : FILE_SEPARATOR_DEFAULT_STRING;
+    }
+    
+    @Override
+    public char getSeparatorChar() {
+        return windowsSeparator ? FILE_SEPARATOR_WINDOWS_CHAR : FILE_SEPARATOR_DEFAULT_CHAR;
+    }
+    
+    @Override
+    public boolean isFile() {
+        return false;
+    }
+    
+    private void checkForFile() {
+        if (!isFile()) {
+            throw new FileIsNotAFileException(getPath() + " is not a file");
+        }
+    }
+    
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
+    
+    private void checkForDirectory() {
+        if (!isDirectory()) {
+            throw new FileIsNotADirectoryException(getPath() + " is not a directory");
+        }
+    }
+    
+    @Override
+    public boolean exists() {
+        return false;
+    }
+    
+    private void checkForExistance() {
+        if (!exists()) {
+            throw new FileIsNotExistingException(getPath() + " does not exist");
+        }
+    }
+    
+    @Override
+    public boolean isAbsolute() {
+        return absolute;
+    }
+    
+    private void checkForAbsolute() {
+        if (!isAbsolute()) {
+            throw new FileIsNotAbsoluteException(getPath() + " is not absolute");
+        }
+    }
+    
+    @Override
+    public boolean isRelative() {
+        return !absolute;
+    }
+    
+    private void checkForRelative() {
+        if (!isRelative()) {
+            throw new FileIsNotRelativeException(getPath() + " is not relative");
+        }
+    }
+    
+    @Override
+    public boolean isIntern() {
+        return !extern;
+    }
+    
+    private void checkForIntern() {
+        if (!isIntern()) {
+            throw new FileIsNotInternException(getPath() + " is not intern");
+        }
+    }
+    
+    @Override
+    public boolean isExtern() {
+        return extern;
+    }
+    
+    private void checkForExtern() {
+        if (!isExtern()) {
+            throw new FileIsNotExternException(getPath() + " is not extern");
+        }
+    }
+    
+    @Override
+    public byte[] readBytes() {
+        return new byte[0];
+    }
+    
+    @Override
+    public boolean writeBytes(byte[] data) {
+        return false;
+    }
+    
+    @Override
+    public boolean mkdir() {
+        return false;
+    }
+    
+    @Override
+    public boolean mkdirs() {
+        return false;
+    }
+    
+    @Override
+    public boolean delete() {
+        return false;
+    }
+    
+    @Override
+    public boolean createNewFile() {
+        return false;
+    }
+    
+    @Override
+    public List<IFile> listFiles(boolean recursive) {
+        return null;
+    }
+    
+}
---
+++ src/main/java/de/codemakers/io/file/t3/IFile.java
@@ -0,0 +1,285 @@
+/*
+ *     Copyright 2018 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.io.file.t3;
+
+import de.codemakers.base.logger.Logger;
+import de.codemakers.io.file.t3.exceptions.FileException;
+import de.codemakers.io.file.t3.exceptions.FileIsNotException;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public interface IFile extends Serializable {
+    
+    String getName();
+    
+    String getPath();
+    
+    String getAbsolutePath();
+    
+    IFile getAbsoluteFile();
+    
+    IFile getParentFile();
+    
+    String getSeparator();
+    
+    char getSeparatorChar();
+    
+    boolean isFile();
+    
+    boolean isDirectory();
+    
+    boolean exists();
+    
+    boolean isAbsolute();
+    
+    boolean isRelative();
+    
+    boolean isIntern();
+    
+    boolean isExtern();
+    
+    Path toPath() throws Exception;
+    
+    URI toURI() throws Exception;
+    
+    URL toURL() throws Exception;
+    
+    boolean mkdir() throws FileIsNotException;
+    
+    default boolean mkdir(Consumer<Throwable> failure) {
+        try {
+            return mkdir();
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean mkdirWithoutException() {
+        return mkdir(null);
+    }
+    
+    boolean mkdirs() throws FileIsNotException;
+    
+    default boolean mkdirs(Consumer<Throwable> failure) {
+        try {
+            return mkdirs();
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean mkdirsWithoutException() {
+        return mkdirs(null);
+    }
+    
+    boolean delete() throws FileException;
+    
+    default boolean delete(Consumer<Throwable> failure) {
+        try {
+            return delete();
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean deleteWithoutException() {
+        return delete(null);
+    }
+    
+    boolean createNewFile() throws FileIsNotException;
+    
+    default boolean createNewFile(Consumer<Throwable> failure) {
+        try {
+            return createNewFile();
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean createNewFileWithoutException() {
+        return createNewFile(null);
+    }
+    
+    byte[] readBytes() throws FileException;
+    
+    default byte[] readBytes(Consumer<Throwable> failure) {
+        try {
+            return readBytes();
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return null;
+        }
+    }
+    
+    default byte[] readBytesWithoutException() {
+        return readBytes(null);
+    }
+    
+    boolean writeBytes(byte[] data) throws FileException;
+    
+    default boolean writeBytes(byte[] data, Consumer<Throwable> failure) {
+        try {
+            return writeBytes(data);
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean writeBytesWithoutException(byte[] data) {
+        return writeBytes(data, null);
+    }
+    
+    default <R> R use(Function<IFile, R> function) throws Exception {
+        if (function == null) {
+            return null;
+        }
+        return function.apply(this);
+    }
+    
+    default <R> R use(Function<IFile, R> function, Consumer<Throwable> failure) {
+        try {
+            return use(function);
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return null;
+        }
+    }
+    
+    default <R> R useWithoutException(Function<IFile, R> function) {
+        return use(function, null);
+    }
+    
+    default List<IFile> listFiles() {
+        return listFiles(false);
+    }
+    
+    List<IFile> listFiles(boolean recursive);
+    
+    default boolean forChildren(Consumer<IFile> consumer) throws Exception {
+        return forChildren(consumer, false);
+    }
+    
+    default boolean forChildren(Consumer<IFile> consumer, boolean recursive) throws Exception {
+        if (consumer == null) {
+            return false;
+        }
+        listFiles(recursive).forEach(consumer);
+        return true;
+    }
+    
+    default boolean forChildren(Consumer<IFile> consumer, Consumer<Throwable> failure) {
+        return forChildren(consumer, false, failure);
+    }
+    
+    default boolean forChildren(Consumer<IFile> consumer, boolean recursive, Consumer<Throwable> failure) {
+        try {
+            return forChildren(consumer, recursive);
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean forChildrenWithoutException(Consumer<IFile> consumer) {
+        return forChildren(consumer, null);
+    }
+    
+    default boolean forChildrenWithoutException(Consumer<IFile> consumer, boolean recursive) {
+        return forChildren(consumer, recursive, null);
+    }
+    
+    default boolean forChildrenParallel(Consumer<IFile> consumer) throws Exception {
+        return forChildrenParallel(consumer, false);
+    }
+    
+    default boolean forChildrenParallel(Consumer<IFile> consumer, boolean recursive) throws Exception {
+        if (consumer == null) {
+            return false;
+        }
+        listFiles(recursive).stream().parallel().forEach(consumer);
+        return true;
+    }
+    
+    default boolean forChildrenParallel(Consumer<IFile> consumer, Consumer<Throwable> failure) {
+        return forChildrenParallel(consumer, false, failure);
+    }
+    
+    default boolean forChildrenParallel(Consumer<IFile> consumer, boolean recursive, Consumer<Throwable> failure) {
+        try {
+            return forChildrenParallel(consumer, recursive);
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.accept(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean forChildrenParallelWithoutException(Consumer<IFile> consumer) {
+        return forChildrenParallelWithoutException(consumer, false);
+    }
+    
+    default boolean forChildrenParallelWithoutException(Consumer<IFile> consumer, boolean recursive) {
+        return forChildrenParallel(consumer, recursive, null);
+    }
+    
+}
