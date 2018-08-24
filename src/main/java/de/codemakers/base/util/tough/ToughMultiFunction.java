7b4ce36352768d6436c245479e4b56c8930f2556
Paul Hagedorn <panzer1119@gmail.com>
2018-08-24 03:20:23 +0200

    Added a MultiFunction.java, which is like a java Function, but with an (theoretically) unlimited number of arguments
    (And the Tough Version of it ToughMultiFunction.java)
    
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/base/util/tough/ToughMultiFunction.java
@@ -0,0 +1,55 @@
+/*
+ *     Copyright 2018 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.base.util.tough;
+
+public interface ToughMultiFunction<T, R> extends Tough<T, R> {
+    
+    R apply(T... ts) throws Exception;
+    
+    default R apply(ToughConsumer<Throwable> failure, T... ts) {
+        try {
+            return apply(ts);
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.acceptWithoutException(ex);
+            } else {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+    }
+    
+    default R applyWithoutException(T... ts) {
+        return apply(null, ts);
+    }
+    
+    @Override
+    default R action(T t) throws Exception {
+        return apply(t);
+    }
+    
+    @Override
+    default boolean canConsume() {
+        return true;
+    }
+    
+    @Override
+    default boolean canSupply() {
+        return true;
+    }
+    
+}
