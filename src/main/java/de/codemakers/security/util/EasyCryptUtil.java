274f6c0508b387e5e3ffbebabd7242d74337107c
Paul Hagedorn <panzer1119@gmail.com>
2018-09-01 03:26:51 +0200

    Added method to verify if an RSA KeyPair is a KeyPair, which is able to Sign/Verify the same data in EasyCryptUtil.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/security/util/EasyCryptUtil.java
+++ src/main/java/de/codemakers/security/util/EasyCryptUtil.java
@@ -16,14 +16,34 @@
 
 package de.codemakers.security.util;
 
+import de.codemakers.base.logger.Logger;
 import de.codemakers.security.interfaces.*;
 import de.codemakers.security.interfaces.Signer;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import java.security.*;
+import java.util.Random;
 
 public class EasyCryptUtil {
+    
+    public static final Signature SIGNATURE_SHA256withRSA;
+    public static final byte[] RANDOM_TEST_BYTES = new byte[32];
+    
+    static {
+        Signature signature = null;
+        try {
+            signature = Signature.getInstance("SHA256withRSA");
+        } catch (Exception ex) {
+            Logger.handleError(ex);
+        }
+        SIGNATURE_SHA256withRSA = signature;
+        try {
+            SecureRandom.getInstanceStrong().nextBytes(RANDOM_TEST_BYTES);
+        } catch (Exception ex) {
+            new Random().nextBytes(RANDOM_TEST_BYTES);
+        }
+    }
     
     public static final Cryptor cryptorOfCipher(Cipher cipher) {
         return (data) -> cipher.doFinal(data);
@@ -79,6 +99,14 @@ public class EasyCryptUtil {
     public static final Verifier verifierOfSignature(Signature signature, PublicKey key) throws InvalidKeyException {
         signature.initVerify(key);
         return verifierOfSignature(signature);
+    }
+    
+    public static final boolean publicKeyCanVerifyDataSignedWithPrivateKeyRSA(PrivateKey privateKey, PublicKey publicKey) throws InvalidKeyException {
+        assert privateKey.getAlgorithm().equals("RSA");
+        assert publicKey.getAlgorithm().equals("RSA");
+        final Signer signer = signerOfSignature(SIGNATURE_SHA256withRSA, privateKey);
+        final Verifier verifier = verifierOfSignature(SIGNATURE_SHA256withRSA, publicKey);
+        return verifier.verifyWithoutException(RANDOM_TEST_BYTES, signer.signWithoutException(RANDOM_TEST_BYTES));
     }
     
 }
