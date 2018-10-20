44cf0e14e3f5f7be28b3f5e64214e2888fbc9d60
Paul Hagedorn <panzer1119@gmail.com>
2018-10-21 00:56:06 +0200

    Updated/Improved ConvertUtil.java ConvertUtil.java now uses ByteBuffer Increased Version to v1.8.0_172-alpha.41
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/base/util/ConvertUtil.java
+++ src/main/java/de/codemakers/base/util/ConvertUtil.java
@@ -16,20 +16,93 @@
 
 package de.codemakers.base.util;
 
+import java.nio.ByteBuffer;
 import java.util.Objects;
 
 public class ConvertUtil {
     
+    public static byte[] doubleToByteArray(double value) {
+        return ByteBuffer.allocate(Double.BYTES).putDouble(value).array();
+    }
+    
+    public static double byteArrayToDouble(byte[] array) {
+        Objects.requireNonNull(array);
+        if (array.length != Double.BYTES) {
+            throw new IllegalArgumentException(String.format("A %s has %d bits, which means %d bytes, not %d bytes", Double.class.getSimpleName(), Double.SIZE, Double.BYTES, array.length));
+        }
+        return ByteBuffer.wrap(array).getDouble();
+    }
+    
     public static byte[] longToByteArray(long value) {
-        return new byte[] {(byte) (value >> 56), (byte) (value >> 48), (byte) (value >> 40), (byte) (value >> 32), (byte) (value >> 24), (byte) (value >> 16), (byte) (value >> 8), (byte) (value)};
+        return ByteBuffer.allocate(Long.BYTES).putLong(value).array();
     }
     
     public static long byteArrayToLong(byte[] array) {
         Objects.requireNonNull(array);
-        if (array.length != 8) {
-            throw new IllegalArgumentException("A Long has 64 bits, which means 8 bytes, not " + array.length + " bytes");
+        if (array.length != Long.BYTES) {
+            throw new IllegalArgumentException(String.format("A %s has %d bits, which means %d bytes, not %d bytes", Long.class.getSimpleName(), Long.SIZE, Long.BYTES, array.length));
         }
-        return ((long) array[0] << 56) | ((long) (array[1] & 0xFF) << 48) | ((long) (array[2] & 0xFF) << 40) | ((long) (array[3] & 0xFF) << 32) | ((long) (array[4] & 0xFF) << 24) | ((long) (array[5] & 0xFF) << 16) | ((long) (array[6] & 0xFF) << 8) | ((long) (array[7] & 0xFF));
+        return ByteBuffer.wrap(array).getLong();
+    }
+    
+    public static byte[] floatToByteArray(float value) {
+        return ByteBuffer.allocate(Float.BYTES).putFloat(value).array();
+    }
+    
+    public static float byteArrayToFloat(byte[] array) {
+        Objects.requireNonNull(array);
+        if (array.length != Float.BYTES) {
+            throw new IllegalArgumentException(String.format("An %s has %d bits, which means %d bytes, not %d bytes", Float.class.getSimpleName(), Float.SIZE, Float.BYTES, array.length));
+        }
+        return ByteBuffer.wrap(array).getFloat();
+    }
+    
+    public static byte[] intToByteArray(int value) {
+        return ByteBuffer.allocate(Integer.BYTES).putLong(value).array();
+    }
+    
+    public static int byteArrayToInt(byte[] array) {
+        Objects.requireNonNull(array);
+        if (array.length != Integer.BYTES) {
+            throw new IllegalArgumentException(String.format("An %s has %d bits, which means %d bytes, not %d bytes", Integer.class.getSimpleName(), Integer.SIZE, Integer.BYTES, array.length));
+        }
+        return ByteBuffer.wrap(array).getInt();
+    }
+    
+    public static byte[] shortToByteArray(short value) {
+        return ByteBuffer.allocate(Short.BYTES).putShort(value).array();
+    }
+    
+    public static short byteArrayToShort(byte[] array) {
+        Objects.requireNonNull(array);
+        if (array.length != Short.BYTES) {
+            throw new IllegalArgumentException(String.format("A %s has %d bits, which means %d bytes, not %d bytes", Short.class.getSimpleName(), Short.SIZE, Short.BYTES, array.length));
+        }
+        return ByteBuffer.wrap(array).getShort();
+    }
+    
+    public static byte[] charToByteArray(char value) {
+        return ByteBuffer.allocate(Character.BYTES).putChar(value).array();
+    }
+    
+    public static char byteArrayToChar(byte[] array) {
+        Objects.requireNonNull(array);
+        if (array.length != Character.BYTES) {
+            throw new IllegalArgumentException(String.format("A %s has %d bits, which means %d bytes, not %d bytes", Character.class.getSimpleName(), Character.SIZE, Character.BYTES, array.length));
+        }
+        return ByteBuffer.wrap(array).getChar();
+    }
+    
+    public static byte[] byteToByteArray(byte value) {
+        return new byte[] {value};
+    }
+    
+    public static byte byteArrayToBytes(byte[] array) {
+        Objects.requireNonNull(array);
+        if (array.length != 1) {
+            throw new IllegalArgumentException(String.format("A %s has %d bits, which means %d bytes, not %d bytes", Byte.class.getSimpleName(), 8, 1, array.length));
+        }
+        return array[0];
     }
     
 }
