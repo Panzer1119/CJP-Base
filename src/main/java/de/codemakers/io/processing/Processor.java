1d5a92ca5f3b396802e3c7712d2d8420df923144
Paul Hagedorn <panzer1119@gmail.com>
2019-01-12 22:00:16 +0100

    Implemented toString() in Processor.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/io/processing/Processor.java
+++ src/main/java/de/codemakers/io/processing/Processor.java
@@ -16,6 +16,8 @@
 
 package de.codemakers.io.processing;
 
+import java.util.Arrays;
+
 public abstract class Processor {
     
     protected byte[] buffer = null;
@@ -42,6 +44,28 @@ public abstract class Processor {
     public Processor reset() {
         buffer = null;
         return this;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Processor processor = (Processor) o;
+        return Arrays.equals(buffer, processor.buffer);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(buffer);
+    }
+    
+    @Override
+    public String toString() {
+        return "Processor{" + "buffer=" + Arrays.toString(buffer) + '}';
     }
     
 }
