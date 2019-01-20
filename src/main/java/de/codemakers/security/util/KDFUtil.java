8103cac3e03f05e4c613604251994212caca2844
Paul Hagedorn <panzer1119@gmail.com>
2019-01-20 22:21:37 +0100

    Created KDFUtil.java (KDF = Key Derivation Function)
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/security/util/KDFUtil.java
@@ -0,0 +1,66 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.security.util;
+
+import de.codemakers.base.logger.Logger;
+
+import javax.crypto.SecretKeyFactory;
+
+public class KDFUtil {
+    
+    public static final String ALGORITHM_PBKDF2WithHmacSHA_1 = "PBKDF2WithHmacSHA1";
+    public static final String ALGORITHM_PBKDF2WithHmacSHA_256 = "PBKDF2WithHmacSHA256";
+    public static final String ALGORITHM_PBKDF2WithHmacSHA_384 = "PBKDF2WithHmacSHA384";
+    public static final String ALGORITHM_PBKDF2WithHmacSHA_512 = "PBKDF2WithHmacSHA512";
+    
+    public static SecretKeyFactory createPBKDF2WithHmacSHA_1() {
+        try {
+            return SecretKeyFactory.getInstance(ALGORITHM_PBKDF2WithHmacSHA_1);
+        } catch (Exception ex) {
+            Logger.handleError(ex);
+            return null;
+        }
+    }
+    
+    public static SecretKeyFactory createPBKDF2WithHmacSHA_256() {
+        try {
+            return SecretKeyFactory.getInstance(ALGORITHM_PBKDF2WithHmacSHA_256);
+        } catch (Exception ex) {
+            Logger.handleError(ex);
+            return null;
+        }
+    }
+    
+    public static SecretKeyFactory createPBKDF2WithHmacSHA_384() {
+        try {
+            return SecretKeyFactory.getInstance(ALGORITHM_PBKDF2WithHmacSHA_384);
+        } catch (Exception ex) {
+            Logger.handleError(ex);
+            return null;
+        }
+    }
+    
+    public static SecretKeyFactory createPBKDF2WithHmacSHA_512() {
+        try {
+            return SecretKeyFactory.getInstance(ALGORITHM_PBKDF2WithHmacSHA_512);
+        } catch (Exception ex) {
+            Logger.handleError(ex);
+            return null;
+        }
+    }
+    
+}
