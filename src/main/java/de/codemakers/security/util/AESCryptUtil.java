437e7d7fc4c654d401c15e28c52e8609c3d4f1c2
Paul Hagedorn <panzer1119@gmail.com>
2019-01-25 22:45:29 +0100

    Added AES/GCM/NoPadding mode to AESCryptUtil.java This is an AES mode which appends an MAC to the encrypted bytes, to support integrity and authentication of a message
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/security/util/AESCryptUtil.java
+++ src/main/java/de/codemakers/security/util/AESCryptUtil.java
@@ -21,6 +21,8 @@ import de.codemakers.security.interfaces.Encryptor;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
 import java.util.Random;
 
 public class AESCryptUtil {
@@ -29,8 +31,10 @@ public class AESCryptUtil {
     
     public static final String MODE_AES = "AES";
     public static final String MODE_AES_CBC_PKCS5Padding = "AES/CBC/PKCS5Padding";
+    public static final String MODE_AES_GCM_NoPadding = "AES/GCM/NoPadding";
     
     public static final int IV_BYTES_CBC = 16;
+    public static final int IV_BYTES_GCM = 12;
     
     public static final Cipher createCipherAES() {
         return EasyCryptUtil.createCipher(MODE_AES);
@@ -40,20 +44,40 @@ public class AESCryptUtil {
         return EasyCryptUtil.createCipher(MODE_AES_CBC_PKCS5Padding);
     }
     
+    public static final Cipher createCipherAESGCMNoPadding() {
+        return EasyCryptUtil.createCipher(MODE_AES_GCM_NoPadding);
+    }
+    
     public static final byte[] generateSecureRandomIVAESCBC() {
         return EasyCryptUtil.generateSecureRandomBytes(IV_BYTES_CBC);
+    }
+    
+    public static final byte[] generateSecureRandomIVAESGCM() {
+        return EasyCryptUtil.generateSecureRandomBytes(IV_BYTES_GCM);
     }
     
     public static final byte[] generateRandomIVAESCBC(Random random) {
         return EasyCryptUtil.generateRandomBytes(IV_BYTES_CBC, random);
     }
     
+    public static final byte[] generateRandomIVAESGCM(Random random) {
+        return EasyCryptUtil.generateRandomBytes(IV_BYTES_GCM, random);
+    }
+    
     public static final Encryptor createEncryptorAESCBCPKCS5Padding(SecretKey secretKey) {
-        return EasyCryptUtil.createEncryptor(createCipherAESCBCPKCS5Padding(), secretKey, true);
+        return EasyCryptUtil.createEncryptor(createCipherAESCBCPKCS5Padding(), secretKey, IvParameterSpec::new);
+    }
+    
+    public static final Encryptor createEncryptorAESGCMNoPadding(SecretKey secretKey, int authentication_tag_bits) {
+        return EasyCryptUtil.createEncryptor(createCipherAESGCMNoPadding(), secretKey, (iv) -> new GCMParameterSpec(authentication_tag_bits, iv));
     }
     
     public static final Decryptor createDecryptorAESCBCPKCS5Padding(SecretKey secretKey) {
-        return EasyCryptUtil.createDecryptor(createCipherAESCBCPKCS5Padding(), secretKey, true);
+        return EasyCryptUtil.createDecryptor(createCipherAESCBCPKCS5Padding(), secretKey, IvParameterSpec::new);
+    }
+    
+    public static final Decryptor createDecryptorAESGCMNoPadding(SecretKey secretKey, int authentication_tag_bits) {
+        return EasyCryptUtil.createDecryptor(createCipherAESGCMNoPadding(), secretKey, (iv) -> new GCMParameterSpec(authentication_tag_bits, iv));
     }
     
 }
