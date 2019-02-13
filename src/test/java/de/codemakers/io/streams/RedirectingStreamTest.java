dda6887e3ff5243ba55ab410dc822a5bd1a30883
Paul Hagedorn <panzer1119@gmail.com>
2019-02-13 13:48:49 +0100

    Created RedirectingStreamTest.java to test RedirectingStream.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/test/java/de/codemakers/io/streams/RedirectingStreamTest.java
@@ -0,0 +1,43 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.io.streams;
+
+import de.codemakers.base.logger.Logger;
+import de.codemakers.io.file.AdvancedFile;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class RedirectingStreamTest {
+    
+    public static final void main(String[] args) throws Exception {
+        final AdvancedFile advancedFile_input = new AdvancedFile("test/test.txt");
+        final AdvancedFile advancedFile_output = new AdvancedFile("test/test.txt.bak");
+        final FileInputStream fileInputStream = (FileInputStream) advancedFile_input.createInputStream();
+        final FileOutputStream fileOutputStream = (FileOutputStream) advancedFile_output.createOutputStream(false);
+        final RedirectingStream<FileInputStream, FileOutputStream> redirectingStream = new RedirectingStream<>(fileInputStream, fileOutputStream);
+        Logger.log("redirectingStream=" + redirectingStream);
+        redirectingStream.setStopOnClose(true);
+        Logger.log("redirectingStream=" + redirectingStream);
+        redirectingStream.start();
+        Logger.log("RedirectingStream started");
+        redirectingStream.createWaiter().waitFor();
+        Logger.log("RedirectingStream finished");
+        Logger.log("redirectingStream=" + redirectingStream);
+    }
+    
+}
