dd0736fcd63db842419e75774da0976accab03cd
Paul Hagedorn <panzer1119@gmail.com>
2019-02-27 23:17:19 +0100

    Created ToughTriFunction.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/base/util/tough/ToughTriFunction.java
@@ -0,0 +1,55 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.base.util.tough;
+
+public interface ToughTriFunction<T, U, V, R> extends Tough<T, R> {
+    
+    R apply(T t, U u, V v) throws Exception;
+    
+    default R apply(T t, U u, V v, ToughConsumer<Throwable> failure) {
+        try {
+            return apply(t, u, v);
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.acceptWithoutException(ex);
+            } else {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+    }
+    
+    default R applyWithoutException(T t, U u, V v) {
+        return apply(t, u, v, null);
+    }
+    
+    @Override
+    default R action(T t) throws Exception {
+        return apply(t, null, null);
+    }
+    
+    @Override
+    default boolean canConsume() {
+        return true;
+    }
+    
+    @Override
+    default boolean canSupply() {
+        return true;
+    }
+    
+}
