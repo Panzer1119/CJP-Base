8c1bf012fee15810cba40c2019f8a3ac38f4fc07
Paul Hagedorn <panzer1119@gmail.com>
2019-03-06 14:00:11 +0100

    Created AdvancedDataOutputStream.java and AdvancedDataInputStream.java This streams additionally can write/read an byte array with its length to the stream, so the receiving site knows the length of the incoming byte array
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/io/streams/AdvancedDataInputStream.java
@@ -0,0 +1,45 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.io.streams;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AdvancedDataInputStream extends DataInputStream {
+    
+    /**
+     * Creates a DataInputStream that uses the specified
+     * underlying InputStream.
+     *
+     * @param in the specified input stream
+     */
+    public AdvancedDataInputStream(InputStream in) {
+        super(in);
+    }
+    
+    public byte[] readBytes() throws IOException {
+        final int length = readInt();
+        if (length < 0) {
+            return null;
+        }
+        final byte[] temp = new byte[length];
+        read(temp);
+        return temp;
+    }
+    
+}
