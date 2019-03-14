ceff24e04a061ab5e4a9e74a43f5f4df719bc358
Paul Hagedorn <panzer1119@gmail.com>
2019-03-14 01:07:27 +0100

    Created Testable.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/base/util/interfaces/Testable.java
@@ -0,0 +1,49 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.base.util.interfaces;
+
+import de.codemakers.base.action.ReturningAction;
+import de.codemakers.base.logger.Logger;
+import de.codemakers.base.util.tough.ToughConsumer;
+
+@FunctionalInterface
+public interface Testable {
+    
+    boolean test() throws Exception;
+    
+    default boolean test(ToughConsumer<Throwable> failure) {
+        try {
+            return test();
+        } catch (Exception ex) {
+            if (failure != null) {
+                failure.acceptWithoutException(ex);
+            } else {
+                Logger.handleError(ex);
+            }
+            return false;
+        }
+    }
+    
+    default boolean testWithoutException() {
+        return test(null);
+    }
+    
+    default ReturningAction<Boolean> testAction() {
+        return new ReturningAction<>(this::test);
+    }
+    
+}
