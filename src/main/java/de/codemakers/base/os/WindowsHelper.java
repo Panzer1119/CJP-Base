abedf32c00b6a390b2beaddecd563d2f6da65357
Paul Hagedorn <panzer1119@gmail.com>
2019-03-22 00:53:33 +0100

    Moved PowerInfo stuff from WindowsHelper.java to WindowsSystemInfo.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/base/os/WindowsHelper.java
+++ src/main/java/de/codemakers/base/os/WindowsHelper.java
@@ -30,40 +30,6 @@ public class WindowsHelper implements OSHelper {
     AtomicLong LAST_ID = new AtomicLong(-1);
     Map<Long, OSFunction> OS_FUNCTIONS = new ConcurrentHashMap<>();
     
-    public static final String DESIGN_CAPACITY = "DesignCapacity";
-    public static final String DESCRIPTION = "Description";
-    public static final String SYSTEM_CREATION_CLASS_NAME = "SystemCreationClassName";
-    public static final String CONFIG_MANAGER_USER_CONFIG = "ConfigManagerUserConfig";
-    public static final String ERROR_CLEARED = "ErrorCleared";
-    public static final String BATTERY_RECHARGE_TIME = "BatteryRechargeTime";
-    public static final String ESTIMATED_CHARGE_REMAINING = "EstimatedChargeRemaining";
-    public static final String NAME = "Name";
-    public static final String SMART_BATTERY_VERSION = "SmartBatteryVersion";
-    public static final String TIME_ON_BATTERY = "TimeOnBattery";
-    public static final String PNP_DEVICE_ID = "PNPDeviceID";
-    public static final String DESIGN_VOLTAGE = "DesignVoltage";
-    public static final String POWER_MANAGEMENT_SUPPORTED = "PowerManagementSupported";
-    public static final String SYSTEM_NAME = "SystemName";
-    public static final String AVAILABILITY = "Availability";
-    public static final String ERROR_DESCRIPTION = "ErrorDescription";
-    public static final String STATUS = "Status";
-    public static final String MAX_RECHARGE_TIME = "MaxRechargeTime";
-    public static final String DEVICE_ID = "DeviceID";
-    public static final String EXPECTED_LIFE = "ExpectedLife";
-    public static final String EXPECTED_BATTERY_LIFE = "ExpectedBatteryLife";
-    public static final String INSTALL_DATE = "InstallDate";
-    public static final String TIME_TO_FULL_CHARGE = "TimeToFullCharge";
-    public static final String CAPTION = "Caption";
-    public static final String CHEMISTRY = "Chemistry";
-    public static final String CONFIG_MANAGER_ERROR_CODE = "ConfigManagerErrorCode";
-    public static final String POWER_MANAGEMENT_CAPABILITIES = "PowerManagementCapabilities";
-    public static final String BATTERY_STATUS = "BatteryStatus";
-    public static final String STATUS_INFO = "StatusInfo";
-    public static final String CREATION_CLASS_NAME = "CreationClassName";
-    public static final String FULL_CHARGE_CAPACITY = "FullChargeCapacity";
-    public static final String LAST_ERROR_CODE = "LastErrorCode";
-    public static final String ESTIMATED_RUN_TIME = "EstimatedRunTime";
-    
     @Override
     public boolean isPathAbsolute(String path) {
         if (path == null || path.isEmpty()) {
--- src/main/java/de/codemakers/base/os/functions/impl/WindowsSystemInfo.java
+++ src/main/java/de/codemakers/base/os/functions/impl/WindowsSystemInfo.java
@@ -30,12 +30,46 @@ import java.util.concurrent.TimeUnit;
 @RegisterOSFunction(supported = {OS.WINDOWS})
 public class WindowsSystemInfo extends SystemInfo {
     
-    public static final String COMMAND_BATTERY_INFO = "WMIC path Win32_Battery";
+    public static final String COMMAND_POWER_INFO = "WMIC path Win32_Battery";
+    
+    public static final String POWER_INFO_BATTERY_DESIGN_CAPACITY = "DesignCapacity";
+    public static final String POWER_INFO_BATTERY_DESCRIPTION = "Description";
+    public static final String POWER_INFO_BATTERY_SYSTEM_CREATION_CLASS_NAME = "SystemCreationClassName";
+    public static final String POWER_INFO_BATTERY_CONFIG_MANAGER_USER_CONFIG = "ConfigManagerUserConfig";
+    public static final String POWER_INFO_BATTERY_ERROR_CLEARED = "ErrorCleared";
+    public static final String POWER_INFO_BATTERY_BATTERY_RECHARGE_TIME = "BatteryRechargeTime";
+    public static final String POWER_INFO_BATTERY_ESTIMATED_CHARGE_REMAINING = "EstimatedChargeRemaining";
+    public static final String POWER_INFO_BATTERY_NAME = "Name";
+    public static final String POWER_INFO_BATTERY_SMART_BATTERY_VERSION = "SmartBatteryVersion";
+    public static final String POWER_INFO_BATTERY_TIME_ON_BATTERY = "TimeOnBattery";
+    public static final String POWER_INFO_BATTERY_PNP_DEVICE_ID = "PNPDeviceID";
+    public static final String POWER_INFO_BATTERY_DESIGN_VOLTAGE = "DesignVoltage";
+    public static final String POWER_INFO_BATTERY_POWER_MANAGEMENT_SUPPORTED = "PowerManagementSupported";
+    public static final String POWER_INFO_BATTERY_SYSTEM_NAME = "SystemName";
+    public static final String POWER_INFO_BATTERY_AVAILABILITY = "Availability";
+    public static final String POWER_INFO_BATTERY_ERROR_DESCRIPTION = "ErrorDescription";
+    public static final String POWER_INFO_BATTERY_STATUS = "Status";
+    public static final String POWER_INFO_BATTERY_MAX_RECHARGE_TIME = "MaxRechargeTime";
+    public static final String POWER_INFO_BATTERY_DEVICE_ID = "DeviceID";
+    public static final String POWER_INFO_BATTERY_EXPECTED_LIFE = "ExpectedLife";
+    public static final String POWER_INFO_BATTERY_EXPECTED_BATTERY_LIFE = "ExpectedBatteryLife";
+    public static final String POWER_INFO_BATTERY_INSTALL_DATE = "InstallDate";
+    public static final String POWER_INFO_BATTERY_TIME_TO_FULL_CHARGE = "TimeToFullCharge";
+    public static final String POWER_INFO_BATTERY_CAPTION = "Caption";
+    public static final String POWER_INFO_BATTERY_CHEMISTRY = "Chemistry";
+    public static final String POWER_INFO_BATTERY_CONFIG_MANAGER_ERROR_CODE = "ConfigManagerErrorCode";
+    public static final String POWER_INFO_BATTERY_POWER_MANAGEMENT_CAPABILITIES = "PowerManagementCapabilities";
+    public static final String POWER_INFO_BATTERY_BATTERY_STATUS = "BatteryStatus";
+    public static final String POWER_INFO_BATTERY_STATUS_INFO = "StatusInfo";
+    public static final String POWER_INFO_BATTERY_CREATION_CLASS_NAME = "CreationClassName";
+    public static final String POWER_INFO_BATTERY_FULL_CHARGE_CAPACITY = "FullChargeCapacity";
+    public static final String POWER_INFO_BATTERY_LAST_ERROR_CODE = "LastErrorCode";
+    public static final String POWER_INFO_BATTERY_ESTIMATED_RUN_TIME = "EstimatedRunTime";
     
     @Override
     public PowerInfo getPowerInfo() throws Exception {
         Logger.getDefaultAdvancedLeveledLogger().setMinimumLogLevel(LogLevel.FINEST);
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec(COMMAND_BATTERY_INFO).getInputStream()));
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec(COMMAND_POWER_INFO).getInputStream()));
         final String line_1 = bufferedReader.readLine();
         bufferedReader.readLine();
         final String line_2 = bufferedReader.readLine();
@@ -45,7 +79,7 @@ public class WindowsSystemInfo extends SystemInfo {
         }
         final Properties properties = new Properties();
         WindowsHelper.process(line_1, line_2, properties);
-        final int batteryState_int = Integer.parseInt(properties.getProperty(WindowsHelper.BATTERY_STATUS));
+        final int batteryState_int = Integer.parseInt(properties.getProperty(POWER_INFO_BATTERY_BATTERY_STATUS));
         BatteryState batteryState = null;
         switch (batteryState_int) {
             case 1:
@@ -81,7 +115,7 @@ public class WindowsSystemInfo extends SystemInfo {
                 batteryState = BatteryState.UNKNOWN;
                 break;
         }
-        return new PowerInfo(properties.getProperty(WindowsHelper.DEVICE_ID, WindowsHelper.DEVICE_ID), properties.getProperty(WindowsHelper.NAME, WindowsHelper.NAME), Double.parseDouble(properties.getProperty(WindowsHelper.ESTIMATED_CHARGE_REMAINING, "0.0")) * 0.01, batteryState, Integer.parseInt(properties.getProperty(WindowsHelper.ESTIMATED_RUN_TIME, "0")), TimeUnit.MINUTES, properties.getProperty(WindowsHelper.TIME_TO_FULL_CHARGE, "").isEmpty() ? -1 : Integer.parseInt(properties.getProperty(WindowsHelper.TIME_TO_FULL_CHARGE, "0")), TimeUnit.MINUTES, PowerSupply.BATTERY, properties);
+        return new PowerInfo(properties.getProperty(POWER_INFO_BATTERY_DEVICE_ID, POWER_INFO_BATTERY_DEVICE_ID), properties.getProperty(POWER_INFO_BATTERY_NAME, POWER_INFO_BATTERY_NAME), Double.parseDouble(properties.getProperty(POWER_INFO_BATTERY_ESTIMATED_CHARGE_REMAINING, "0.0")) * 0.01, batteryState, Integer.parseInt(properties.getProperty(POWER_INFO_BATTERY_ESTIMATED_RUN_TIME, "0")), TimeUnit.MINUTES, properties.getProperty(POWER_INFO_BATTERY_TIME_TO_FULL_CHARGE, "").isEmpty() ? -1 : Integer.parseInt(properties.getProperty(POWER_INFO_BATTERY_TIME_TO_FULL_CHARGE, "0")), TimeUnit.MINUTES, PowerSupply.BATTERY, properties);
     }
     
 }
