6b407a3b69fed17e15555046a9a4b9cc2e9f28af
Paul Hagedorn <panzer1119@gmail.com>
2019-03-21 14:39:45 +0100

    Created UpdatableBoundResettableVariable.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/base/entities/UpdatableBoundResettableVariable.java
@@ -0,0 +1,74 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.base.entities;
+
+import de.codemakers.base.util.interfaces.Updatable;
+import de.codemakers.base.util.tough.ToughConsumer;
+import de.codemakers.base.util.tough.ToughSupplier;
+
+public class UpdatableBoundResettableVariable<T> extends BoundResettableVariable<T> implements Updatable<Boolean, Void> {
+    
+    protected ToughSupplier<T> toughSupplier;
+    
+    public UpdatableBoundResettableVariable(ToughConsumer<T> toughConsumer) {
+        this((T) null, toughConsumer);
+    }
+    
+    public UpdatableBoundResettableVariable(T current, ToughConsumer<T> toughConsumer) {
+        this(current, () -> current, toughConsumer);
+    }
+    
+    public UpdatableBoundResettableVariable(T current, T temp, ToughConsumer<T> toughConsumer) {
+        this(current, temp, () -> current, toughConsumer);
+    }
+    
+    public UpdatableBoundResettableVariable(ToughSupplier<T> toughSupplier, ToughConsumer<T> toughConsumer) {
+        super(toughSupplier.getWithoutException(), toughConsumer);
+        this.toughSupplier = toughSupplier;
+    }
+    
+    public UpdatableBoundResettableVariable(T current, ToughSupplier<T> toughSupplier, ToughConsumer<T> toughConsumer) {
+        super(current, toughConsumer);
+        this.toughSupplier = toughSupplier;
+    }
+    
+    public UpdatableBoundResettableVariable(T current, T temp, ToughSupplier<T> toughSupplier, ToughConsumer<T> toughConsumer) {
+        super(current, temp, toughConsumer);
+        this.toughSupplier = toughSupplier;
+    }
+    
+    public ToughSupplier<T> getToughSupplier() {
+        return toughSupplier;
+    }
+    
+    public UpdatableBoundResettableVariable setToughSupplier(ToughSupplier<T> toughSupplier) {
+        this.toughSupplier = toughSupplier;
+        return this;
+    }
+    
+    @Override
+    public Boolean update(Void aVoid) throws Exception {
+        setCurrent(toughSupplier.get());
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "UpdatableBoundResettableVariable{" + "toughSupplier=" + toughSupplier + ", toughConsumer=" + toughConsumer + ", current=" + current + ", temp=" + temp + '}';
+    }
+    
+}
