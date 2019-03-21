1fed8dcfe9da613ef549e90e5478eb774e216733
Paul Hagedorn <panzer1119@gmail.com>
2019-03-21 23:50:20 +0100

    Tested new direct invocation of OSFunctions with OSUtil.java in WindowsSpeakTextTest.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/test/java/de/codemakers/misc/os/functions/WindowsSpeakTextTest.java
+++ src/test/java/de/codemakers/misc/os/functions/WindowsSpeakTextTest.java
@@ -27,8 +27,12 @@ public class WindowsSpeakTextTest {
         final SpeakText speakText = OSUtil.getFunction(SpeakText.class);
         Logger.log("speakText=" + speakText);
         if (speakText != null) {
-            Logger.log("spoken: " + speakText.speak("Hallo dies ist ein Test!"));
+            Logger.log("spoken 1: " + speakText.speak("Hallo dies ist ein Test!"));
+            //final String text = "Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende Freude nach sich zieht? Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende Freude nach sich zieht?Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur,";
+            final String text = "Ich bin ein Text!";
+            Logger.log("spoken 2: " + speakText.speak(text));
         }
+        Logger.log("spoken 3: " + OSUtil.invokeFunction(SpeakText.class, (speakText_) -> speakText_.speak("Ich wurde direkt invoked!")));
     }
     
 }
