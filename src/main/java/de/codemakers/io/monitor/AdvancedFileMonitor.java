1c9cb2642c0e3e29ba7eb3d21eb4147c8daae020
Paul Hagedorn <panzer1119@gmail.com>
2019-04-17 22:07:28 +0200

    Created AdvancedFileMonitor.java (extending AbstractMonitor.java)
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/io/monitor/AdvancedFileMonitor.java
@@ -0,0 +1,61 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.io.monitor;
+
+import de.codemakers.base.util.HashUtil;
+import de.codemakers.base.util.interfaces.Hasher;
+import de.codemakers.base.util.monitor.AbstractMonitor;
+
+import java.util.Objects;
+
+public class AdvancedFileMonitor extends AbstractMonitor {
+    
+    protected Hasher hasher;
+    
+    public AdvancedFileMonitor() {
+        this(HashUtil.createHasher64XX());
+    }
+    
+    public AdvancedFileMonitor(Hasher hasher) {
+        this.hasher = Objects.requireNonNull(hasher, "hasher");
+    }
+    
+    public Hasher getHasher() {
+        return hasher;
+    }
+    
+    public AdvancedFileMonitor setHasher(Hasher hasher) {
+        this.hasher = hasher;
+        return this;
+    }
+    
+    @Override
+    public boolean start() throws Exception {
+        return false;
+    }
+    
+    @Override
+    public boolean stop() throws Exception {
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        return "AdvancedFileMonitor{" + "period=" + period + '}';
+    }
+    
+}
