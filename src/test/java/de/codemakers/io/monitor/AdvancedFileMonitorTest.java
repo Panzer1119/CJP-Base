10f4b6f8010177b0f9cd350969bea981ba5efd78
Paul Hagedorn <panzer1119@gmail.com>
2019-04-17 23:25:09 +0200

    Edited AdvancedFileMonitorTest.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/test/java/de/codemakers/io/monitor/AdvancedFileMonitorTest.java
+++ src/test/java/de/codemakers/io/monitor/AdvancedFileMonitorTest.java
@@ -16,10 +16,70 @@
 
 package de.codemakers.io.monitor;
 
+import de.codemakers.base.Standard;
+import de.codemakers.base.logger.LogLevel;
+import de.codemakers.base.logger.Logger;
+import de.codemakers.io.file.AdvancedFile;
+import de.codemakers.io.listeners.AdvancedFileChangeListener;
+
 public class AdvancedFileMonitorTest {
     
     public static final void main(String[] args) throws Exception {
-        final AdvancedFileMonitor advancedFileMonitor = new AdvancedFileMonitor(null);
+        Logger.getDefaultAdvancedLeveledLogger().setMinimumLogLevel(LogLevel.FINER);
+        final AdvancedFile root = new AdvancedFile(args[0]);
+        Logger.log("root=" + root);
+        final AdvancedFileMonitor advancedFileMonitor = new AdvancedFileMonitor(root);
+        Logger.log("advancedFileMonitor=" + advancedFileMonitor);
+        advancedFileMonitor.addAdvancedFileChangeListeners(new AdvancedFileChangeListener() {
+            @Override
+            public void onFileCreated(AdvancedFile file) {
+                Logger.logDebug("onFileCreated:" + file);
+            }
+    
+            @Override
+            public void onFileModified(AdvancedFile file) {
+                Logger.logDebug("onFileModified:" + file);
+            }
+    
+            @Override
+            public void onFileDeleted(AdvancedFile file) {
+                Logger.logDebug("onFileDeleted:" + file);
+            }
+    
+            @Override
+            public void onFileRenamed(AdvancedFile file) {
+                Logger.logDebug("onFileRenamed:" + file);
+            }
+    
+            @Override
+            public void onDirectoryCreated(AdvancedFile directory) {
+                Logger.logDebug("onDirectoryCreated:" + directory);
+            }
+    
+            @Override
+            public void onDirectoryModified(AdvancedFile directory) {
+                Logger.logDebug("onDirectoryModified:" + directory);
+            }
+    
+            @Override
+            public void onDirectoryDeleted(AdvancedFile directory) {
+                Logger.logDebug("onDirectoryDeleted:" + directory);
+            }
+    
+            @Override
+            public void onDirectoryRenamed(AdvancedFile directory) {
+                Logger.logDebug("onDirectoryRenamed:" + directory);
+            }
+        });
+        Logger.log("advancedFileMonitor=" + advancedFileMonitor);
+        advancedFileMonitor.start();
+        Logger.log("advancedFileMonitor=" + advancedFileMonitor);
+        Standard.addShutdownHook(advancedFileMonitor::stop);
+        Standard.async(() -> {
+            Thread.sleep(10000);
+            Logger.log("Exiting");
+            System.exit(0);
+        });
     }
     
 }
