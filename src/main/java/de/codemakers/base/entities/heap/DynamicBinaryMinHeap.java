ffc28fd80941fa2f69b65f3ba7bc894d2876d882
Paul Hagedorn <panzer1119@gmail.com>
2019-10-22 18:01:56 +0200

    Created DynamicBinaryMinHeap.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/base/entities/heap/DynamicBinaryMinHeap.java
@@ -0,0 +1,53 @@
+/*
+ *     Copyright 2018 - 2019 Paul Hagedorn (Panzer1119)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package de.codemakers.base.entities.heap;
+
+import java.util.Arrays;
+
+public class DynamicBinaryMinHeap<E extends Comparable<E>> extends BinaryMinHeap<E> {
+    
+    public DynamicBinaryMinHeap() {
+        this(10);
+    }
+    
+    public DynamicBinaryMinHeap(int capacity) {
+        super(capacity);
+    }
+    
+    @Override
+    public boolean add(E e) {
+        if (isFull()) {
+            heap = Arrays.copyOf(heap, heap.length + 1);
+        }
+        return super.add(e);
+    }
+    
+    @Override
+    public E remove(int i) {
+        final E removed = super.remove(i);
+        if (heap.length >= 1) {
+            heap = Arrays.copyOf(heap, heap.length - 1);
+        }
+        return removed;
+    }
+    
+    @Override
+    public String toString() {
+        return "DynamicBinaryMinHeap{" + "heap=" + Arrays.toString(heap) + ", heapSize=" + heapSize + '}';
+    }
+    
+}
