23431198ad367a6f8d37c9eb42f335ec3c20084c
Paul Hagedorn <panzer1119@gmail.com>
2021-08-03 06:50:57 +0200

    feat: create HibernateProperties.java, add templates hibernate-mysql.properties and hibernate-postgresql.properties

---
+++ src/main/java/de/codemakers/database/hibernate/HibernateProperties.java
@@ -0,0 +1,171 @@
+/*
+ *    Copyright 2021 Paul Hagedorn (Panzer1119)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package de.codemakers.database.hibernate;
+
+import de.codemakers.base.util.DefaultSettings;
+import de.codemakers.base.util.Settings;
+import de.codemakers.io.IOUtil;
+import org.apache.commons.text.StringSubstitutor;
+import org.hibernate.dialect.Dialect;
+
+import java.util.Properties;
+
+public class HibernateProperties {
+    
+    public static final String RESOURCE_PATH_TEMPLATE_MYSQL = "/de/codemakers/database/hibernate-mysql.properties";
+    public static final String RESOURCE_PATH_TEMPLATE_POSTGRESQL = "/de/codemakers/database/hibernate-postgresql.properties";
+    
+    private static final DefaultSettings TEMPLATE_MYSQL = new DefaultSettings();
+    private static final DefaultSettings TEMPLATE_POSTGRESQL = new DefaultSettings();
+    
+    // // Config Keys
+    // Misc Stuff
+    public static final String KEY_HIBERNATE_DIALECT = "hibernate.dialect";
+    // Audit Table Stuff
+    public static final String KEY_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_PREFIX = "org.hibernate.envers.audit_table_prefix";
+    public static final String KEY_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_SUFFIX = "org.hibernate.envers.audit_table_suffix";
+    // Connection Stuff
+    public static final String KEY_HIBERNATE_CONNECTION_URL = "hibernate.connection.url";
+    public static final String KEY_HIBERNATE_CONNECTION_USERNAME = "hibernate.connection.username";
+    public static final String KEY_HIBERNATE_CONNECTION_PASSWORD = "hibernate.connection.password";
+    public static final String KEY_DEFAULT_PORT = "default.port";
+    // // Config Default Values
+    // Misc Stuff
+    public static final String DEFAULT_HIBERNATE_DIALECT = null;
+    // Audit Table Stuff
+    public static final String DEFAULT_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_PREFIX = "audit_log_";
+    public static final String DEFAULT_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_SUFFIX = "";
+    // Connection Stuff
+    public static final String DEFAULT_HIBERNATE_CONNECTION_URL = null;
+    public static final String DEFAULT_HIBERNATE_CONNECTION_USERNAME = null;
+    public static final String DEFAULT_HIBERNATE_CONNECTION_PASSWORD = null;
+    public static final String DEFAULT_DEFAULT_PORT = null;
+    
+    // // Substitution Keys
+    // Connection Stuff
+    public static final String KEY_HOST = "host";
+    public static final String KEY_PORT = "port";
+    public static final String KEY_DATABASE = "database";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    
+    static {
+        loadTemplate(RESOURCE_PATH_TEMPLATE_MYSQL, TEMPLATE_MYSQL);
+        loadTemplate(RESOURCE_PATH_TEMPLATE_POSTGRESQL, TEMPLATE_POSTGRESQL);
+    }
+    
+    private static void loadTemplate(String resourcePath, Settings settings) {
+        IOUtil.getResourceAsStream(resourcePath).ifPresent(settings::loadSettings);
+    }
+    
+    public static HibernateProperties createMySQLTemplate() {
+        return new HibernateProperties((Properties) TEMPLATE_MYSQL.getProperties().clone());
+    }
+    
+    public static HibernateProperties createPostgreSQLTemplate() {
+        return new HibernateProperties((Properties) TEMPLATE_POSTGRESQL.getProperties().clone());
+    }
+    
+    public static Properties createSubstitutedProperties(Properties template, Properties substitutions) {
+        final Properties properties = new Properties();
+        properties.putAll(template);
+        properties.stringPropertyNames().forEach((key) -> substitute(properties, key, substitutions));
+        return properties;
+    }
+    
+    private static void substitute(Properties properties, String key, Properties substitutions) {
+        final String value = properties.getProperty(key);
+        final String replaced = StringSubstitutor.replace(value, substitutions);
+        properties.setProperty(key, replaced);
+    }
+    
+    private static void checkDefaultValues(Properties properties) {
+        checkDefaultValue(properties, KEY_HIBERNATE_DIALECT, DEFAULT_HIBERNATE_DIALECT);
+        checkDefaultValue(properties, KEY_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_PREFIX, DEFAULT_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_PREFIX);
+        checkDefaultValue(properties, KEY_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_SUFFIX, DEFAULT_ORG_HIBERNATE_ENVERS_AUDIT_TABLE_SUFFIX);
+        checkDefaultValue(properties, KEY_HIBERNATE_CONNECTION_URL, DEFAULT_HIBERNATE_CONNECTION_URL);
+        checkDefaultValue(properties, KEY_HIBERNATE_CONNECTION_USERNAME, DEFAULT_HIBERNATE_CONNECTION_USERNAME);
+        checkDefaultValue(properties, KEY_HIBERNATE_CONNECTION_PASSWORD, DEFAULT_HIBERNATE_CONNECTION_PASSWORD);
+    }
+    
+    private static void checkDefaultValue(Properties properties, String key, String defaultValue) {
+        if (!properties.containsKey(key)) {
+            properties.setProperty(key, defaultValue);
+        }
+    }
+    
+    private final Properties basic;
+    private final Properties substitutions;
+    
+    public HibernateProperties(Properties basic) {
+        this(basic, new Properties());
+    }
+    
+    public HibernateProperties(Properties basic, Properties substitutions) {
+        this.basic = basic;
+        this.substitutions = substitutions;
+    }
+    
+    public Properties createProperties() {
+        if (!substitutions.containsKey(KEY_PORT)) {
+            if (!basic.containsKey(KEY_DEFAULT_PORT)) {
+                throw new IllegalArgumentException("Missing port definition and default port definition");
+            }
+            substitutions.setProperty(KEY_PORT, basic.getProperty(KEY_DEFAULT_PORT));
+        }
+        final Properties substitutedProperties = createSubstitutedProperties(basic, substitutions);
+        checkDefaultValues(substitutedProperties);
+        return substitutedProperties;
+    }
+    
+    public HibernateProperties setDialect(Class<? extends Dialect> dialect) {
+        basic.setProperty(KEY_HIBERNATE_DIALECT, dialect.getName());
+        return this;
+    }
+    
+    public HibernateProperties setHost(String host) {
+        substitutions.setProperty(KEY_HOST, host);
+        return this;
+    }
+    
+    public HibernateProperties setPort(Integer port) {
+        if (port == null || port < 0) {
+            if (!basic.containsKey(KEY_DEFAULT_PORT)) {
+                throw new IllegalArgumentException("Empty port definition and missing default port definition");
+            }
+            port = Integer.parseInt(basic.getProperty(KEY_PORT));
+        }
+        substitutions.setProperty(KEY_PORT, "" + port);
+        return this;
+    }
+    
+    public HibernateProperties setDatabase(String database) {
+        substitutions.setProperty(KEY_DATABASE, database);
+        return this;
+    }
+    
+    public HibernateProperties setUsername(String username) {
+        substitutions.setProperty(KEY_USERNAME, username);
+        return this;
+    }
+    
+    public HibernateProperties setPassword(String password) {
+        substitutions.setProperty(KEY_PASSWORD, password);
+        return this;
+    }
+    
+}
