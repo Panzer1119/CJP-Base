8ddaa5fe6153396133b7f71041072958272d2d3a
Paul Hagedorn <panzer1119@gmail.com>
2021-08-08 22:56:01 +0200

    feat(i18n): add custom ResourceBundle support to I18nUtil.java

--- src/main/java/de/codemakers/i18n/I18nUtil.java
+++ src/main/java/de/codemakers/i18n/I18nUtil.java
@@ -19,7 +19,9 @@ package de.codemakers.i18n;
 import de.codemakers.base.Standard;
 
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class I18nUtil {
     
@@ -28,19 +30,40 @@ public class I18nUtil {
     private static final String PATH_LOG_LEVEL = PATH_BASE + "log_level";
     private static final String PATH_UI = PATH_BASE + "ui";
     
+    private static volatile Locale LOCALE = null;
+    
     private static ResourceBundle RESOURCE_BUNDLE_CONSOLE;
     private static ResourceBundle RESOURCE_BUNDLE_LOG_LEVEL;
     private static ResourceBundle RESOURCE_BUNDLE_UI;
+    private static final Map<String, ResourceBundle> RESOURCE_BUNDLES = new ConcurrentHashMap<>();
     
     static {
-        loadResourceBundles(Locale.getDefault());
+        setLocale(Locale.getDefault());
     }
     
-    public static void loadResourceBundles(Locale locale) {
-        RESOURCE_BUNDLE_CONSOLE = ResourceBundle.getBundle(PATH_CONSOLE, locale);
-        RESOURCE_BUNDLE_LOG_LEVEL = ResourceBundle.getBundle(PATH_LOG_LEVEL, locale);
-        RESOURCE_BUNDLE_UI = ResourceBundle.getBundle(PATH_UI, locale);
-        I18nReloadEventHandler.triggerReload(locale);
+    public static synchronized void setLocale(Locale locale) {
+        LOCALE = locale;
+        loadResourceBundles();
+        loadCustomResourceBundles();
+        I18nReloadEventHandler.triggerReload(LOCALE);
+    }
+    
+    private static synchronized void loadResourceBundles() {
+        RESOURCE_BUNDLE_CONSOLE = loadResourceBundle(PATH_CONSOLE);
+        RESOURCE_BUNDLE_LOG_LEVEL = loadResourceBundle(PATH_LOG_LEVEL);
+        RESOURCE_BUNDLE_UI = loadResourceBundle(PATH_UI);
+    }
+    
+    private static synchronized ResourceBundle loadResourceBundle(String path) {
+        return ResourceBundle.getBundle(path, LOCALE);
+    }
+    
+    private static synchronized void loadCustomResourceBundles() {
+        RESOURCE_BUNDLES.keySet().forEach(I18nUtil::loadCustomResourceBundle);
+    }
+    
+    private static synchronized void loadCustomResourceBundle(String path) {
+        RESOURCE_BUNDLES.put(path, loadResourceBundle(path));
     }
     
     public static ResourceBundle getResourceBundleConsole() {
@@ -65,6 +88,14 @@ public class I18nUtil {
     
     public static void triggerReload(Locale locale) {
         I18nReloadEventHandler.triggerReload(locale);
+    }
+    
+    public static void registerResourceBundle(String path) {
+        loadCustomResourceBundle(path);
+    }
+    
+    public static ResourceBundle getResourceBundle(String path) {
+        return RESOURCE_BUNDLES.get(path);
     }
     
 }
